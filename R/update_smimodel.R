52ee110556a6a7319540085b193279ae27833510
Nuwani Palihawadana <nuwanipalihawadana@gmail.com>
2023-12-28 17:28:04 +1100

    Fix errors in (q+1) index step

--- R/update_smimodel.R
+++ R/update_smimodel.R
@@ -74,97 +74,109 @@ update_smimodel <- function(object, data, lambda0 = 1, lambda2 = 1,
     j <- length(ind_pos_current)+1
     num_pred <- length(object$vars_index)
     while(j <= num_pred){
-      # Calculating indices
-      ind <- vector(length = length(ind_pos_current), mode = "list")
-      for(i in 1:length(ind)){
-        ind[[i]] <- as.numeric(X_new_current[, ind_pos_current[[i]]] %*% 
-                                 as.matrix(alpha_current[ind_pos_current[[i]]], ncol = 1))
-      }
-      dat_names <- names(ind) <- paste0("index", 1:length(ind))
-      dat <- as_tibble(ind)
-      # Nonlinear function update 
-      # Constructing the formula
-      pre.formula <- lapply(dat_names, function(var) paste0("s(", var, ',bs="cr")')) %>%
-        paste(collapse = "+") %>% 
-        paste(object$var_y, "~", .)
-      if (!is.null(object$vars_linear)){
-        pre.formula <- lapply(object$vars_linear, function(var) paste0(var)) %>%
-          paste(collapse = "+") %>% 
-          paste(pre.formula, "+", .)
-      }
-      # Model fitting
-      dat <- dplyr::bind_cols(data, dat)
-      fun1 <- mgcv::gam(as.formula(pre.formula), data = dat, method = "REML")
-      Yhat <- as.matrix(fun1$fitted.values, ncol = 1, nrow = length(fun1$fitted.values))
-      # Residuals (R) matrix
-      R <- as.matrix(data[ , object$var_y] - Yhat)
-      # Initialising new index to be added to the current model
+      # Checking whether a new index can be added without variable repetition;
+      # if not, terminating the loop.
       index_list <- split(alpha_current, index_current)
-      if(length(index_list) == 1){
-        index_mat <- as.matrix(bind_rows(index_list)) 
-      }else{
-        index_mat <- t(as.matrix(bind_rows(index_list)))
-      }
+      index_mat <- do.call(rbind, index_list)
       drop_pred_ind <- which(colSums(index_mat) == 0)
-      drop_pred_name <- object$vars_index[drop_pred_ind]
-      X_init <- as.matrix(data[, drop_pred_name])
-      index.ind <- rep(1, length(drop_pred_name))
-      coefs_init <- init_alpha(Y = R, X = X_init, index.ind = index.ind,
-                               init.type = "reg")$alpha_init
-      new_ind <- numeric(length = num_pred)
-      new_ind[drop_pred_ind] <- coefs_init
-      alpha <- c(alpha_current, new_ind)
-      X_index <- data[ , object$vars_index]
-      # Adjusting X (matrix of predictors) to fit number of indices
-      X_new <- as.matrix(do.call(cbind, replicate(j, X_index, simplify = FALSE)))
-      index <- vector(mode = "list", length = j)
-      for(i in 1:j){
-        index[[i]] <- rep(i, num_pred)
-      }
-      index <- unlist(index)
-      ind_pos <- split(1:length(index), index)
-      # Calculating indices
-      ind <- vector(length = length(ind_pos), mode = "list")
-      for(i in 1:length(ind)){
-        ind[[i]] <- as.numeric(X_new[, ind_pos[[i]]] %*% 
-                                 as.matrix(alpha[ind_pos[[i]]], ncol = 1))
-      }
-      dat_names <- names(ind) <- paste0("index", 1:length(ind))
-      dat <- as_tibble(ind)
-      # Nonlinear function update 
-      # Constructing the formula
-      pre.formula <- lapply(dat_names, function(var) paste0("s(", var, ',bs="cr")')) %>%
-        paste(collapse = "+") %>% 
-        paste(object$var_y, "~", .)
-      if (!is.null(object$vars_linear)){
-        pre.formula <- lapply(object$vars_linear, function(var) paste0(var)) %>%
+      if(length(drop_pred_ind) == 0){
+        break
+      }else{
+        # Initialising the new index to be added to the current model
+        drop_pred_name <- object$vars_index[drop_pred_ind]
+        X_init <- as.matrix(data[, drop_pred_name])
+        index.ind <- rep(1, length(drop_pred_name))
+        # Calculating indices
+        ind <- vector(length = length(ind_pos_current), mode = "list")
+        for(i in 1:length(ind)){
+          ind[[i]] <- as.numeric(X_new_current[, ind_pos_current[[i]]] %*% 
+                                   as.matrix(alpha_current[ind_pos_current[[i]]], ncol = 1))
+        }
+        dat_names <- names(ind) <- paste0("index", 1:length(ind))
+        dat <- as_tibble(ind)
+        # Nonlinear function update 
+        # Constructing the formula
+        pre.formula <- lapply(dat_names, function(var) paste0("s(", var, ',bs="cr")')) %>%
           paste(collapse = "+") %>% 
-          paste(pre.formula, "+", .)
-      }
-      # Model fitting
-      dat <- dplyr::bind_cols(data, dat)
-      gam2 <- mgcv::gam(as.formula(pre.formula), data = dat, method = "REML")
-      # Derivatives of the fitted smooths
-      dgz <- vector(length = length(dat_names), mode = "list")
-      for (i in seq_along(dat_names)) {
-        temp <- gratia::derivatives(gam2, type = "central", data = dat, 
-                                    term = paste0("s(", paste0(dat_names[i]), ")"))
-        dgz[[i]] <- temp$derivative
-      }
-      names(dgz) <- paste0("d", seq_along(dat_names))
-      dgz <- as.matrix(as_tibble(dgz))
-      # Optimising the new model
-      best_alpha2 <- inner_update(x = gam2, data = data, yvar = object$var_y, 
-                                  index.vars = object$vars_index, 
-                                  linear.vars = object$vars_linear, 
-                                  num_ind = j, dgz = dgz, 
-                                  alpha_old = alpha, lambda0 = lambda0, 
-                                  lambda2 = lambda2, M = M, max.iter = max.iter,
-                                  tol = tol, TimeLimit = TimeLimit, verbose = verbose)
-      # Termination/continuation checks
-      if((length(ind_pos_current) == length(best_alpha2$ind_pos))){
-        comparison <- abs(alpha_current - best_alpha2$best_alpha) <= tolCoefs
-        if(all(comparison)){
+          paste(object$var_y, "~", .)
+        if (!is.null(object$vars_linear)){
+          pre.formula <- lapply(object$vars_linear, function(var) paste0(var)) %>%
+            paste(collapse = "+") %>% 
+            paste(pre.formula, "+", .)
+        }
+        # Model fitting
+        dat <- dplyr::bind_cols(data, dat)
+        fun1 <- mgcv::gam(as.formula(pre.formula), data = dat, method = "REML")
+        Yhat <- as.matrix(fun1$fitted.values, ncol = 1, nrow = length(fun1$fitted.values))
+        # Residuals (R) matrix
+        R <- as.matrix(data[ , object$var_y] - Yhat)
+        coefs_init <- init_alpha(Y = R, X = X_init, index.ind = index.ind,
+                                 init.type = "reg")$alpha_init
+        new_ind <- numeric(length = num_pred)
+        new_ind[drop_pred_ind] <- coefs_init
+        alpha <- c(alpha_current, new_ind)
+        X_index <- data[ , object$vars_index]
+        # Adjusting X (matrix of predictors) to fit number of indices
+        X_new <- as.matrix(do.call(cbind, replicate(j, X_index, simplify = FALSE)))
+        index <- vector(mode = "list", length = j)
+        for(i in 1:j){
+          index[[i]] <- rep(i, num_pred)
+        }
+        index <- unlist(index)
+        ind_pos <- split(1:length(index), index)
+        # Calculating indices
+        ind <- vector(length = length(ind_pos), mode = "list")
+        for(i in 1:length(ind)){
+          ind[[i]] <- as.numeric(X_new[, ind_pos[[i]]] %*% 
+                                   as.matrix(alpha[ind_pos[[i]]], ncol = 1))
+        }
+        dat_names <- names(ind) <- paste0("index", 1:length(ind))
+        dat <- as_tibble(ind)
+        # Nonlinear function update 
+        # Constructing the formula
+        pre.formula <- lapply(dat_names, function(var) paste0("s(", var, ',bs="cr")')) %>%
+          paste(collapse = "+") %>% 
+          paste(object$var_y, "~", .)
+        if (!is.null(object$vars_linear)){
+          pre.formula <- lapply(object$vars_linear, function(var) paste0(var)) %>%
+            paste(collapse = "+") %>% 
+            paste(pre.formula, "+", .)
+        }
+        # Model fitting
+        dat <- dplyr::bind_cols(data, dat)
+        gam2 <- mgcv::gam(as.formula(pre.formula), data = dat, method = "REML")
+        # Derivatives of the fitted smooths
+        dgz <- vector(length = length(dat_names), mode = "list")
+        for (i in seq_along(dat_names)) {
+          temp <- gratia::derivatives(gam2, type = "central", data = dat, 
+                                      term = paste0("s(", paste0(dat_names[i]), ")"))
+          dgz[[i]] <- temp$derivative
+        }
+        names(dgz) <- paste0("d", seq_along(dat_names))
+        dgz <- as.matrix(as_tibble(dgz))
+        # Optimising the new model
+        best_alpha2 <- inner_update(x = gam2, data = data, yvar = object$var_y, 
+                                    index.vars = object$vars_index, 
+                                    linear.vars = object$vars_linear, 
+                                    num_ind = j, dgz = dgz, 
+                                    alpha_old = alpha, lambda0 = lambda0, 
+                                    lambda2 = lambda2, M = M, max.iter = max.iter,
+                                    tol = tol, TimeLimit = TimeLimit, verbose = verbose)
+        # Termination/continuation checks
+        if((length(ind_pos_current) == length(best_alpha2$ind_pos))){
+          comparison <- abs(alpha_current - best_alpha2$best_alpha) <= tolCoefs
+          if(all(comparison)){
+            if(best_alpha2$min_loss >= loss_current){
+              break
+            }else{
+              alpha_current <- best_alpha2$best_alpha
+              loss_current <- best_alpha2$min_loss
+              index_current <- best_alpha2$index.ind
+              ind_pos_current <- best_alpha2$ind_pos
+              X_new_current <- best_alpha2$X_new
+              break
+            }
+          }
           if(best_alpha2$min_loss >= loss_current){
             break
           }else{
@@ -173,30 +185,20 @@ update_smimodel <- function(object, data, lambda0 = 1, lambda2 = 1,
             index_current <- best_alpha2$index.ind
             ind_pos_current <- best_alpha2$ind_pos
             X_new_current <- best_alpha2$X_new
+          }
+        }else{
+          if(best_alpha2$min_loss >= loss_current){
             break
+          }else{
+            alpha_current <- best_alpha2$best_alpha
+            loss_current <- best_alpha2$min_loss
+            index_current <- best_alpha2$index.ind
+            ind_pos_current <- best_alpha2$ind_pos
+            X_new_current <- best_alpha2$X_new
           }
         }
-        if(best_alpha2$min_loss >= loss_current){
-          break
-        }else{
-          alpha_current <- best_alpha2$best_alpha
-          loss_current <- best_alpha2$min_loss
-          index_current <- best_alpha2$index.ind
-          ind_pos_current <- best_alpha2$ind_pos
-          X_new_current <- best_alpha2$X_new
-        }
-      }else{
-        if(best_alpha2$min_loss >= loss_current){
-          break
-        }else{
-          alpha_current <- best_alpha2$best_alpha
-          loss_current <- best_alpha2$min_loss
-          index_current <- best_alpha2$index.ind
-          ind_pos_current <- best_alpha2$ind_pos
-          X_new_current <- best_alpha2$X_new
-        }
+        j <- length(ind_pos_current)+1
       }
-      j <- length(ind_pos_current)+1
     }
     # Calculating indices
     ind <- vector(length = length(ind_pos_current), mode = "list")

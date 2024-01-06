d1eec7d1dcea314a64c6f453a54be7a9c1e602ba
Nuwani Palihawadana <nuwanipalihawadana@gmail.com>
2024-01-06 14:00:16 +1100

    Add helper function smimodel()

---
+++ R/smimodel.R
@@ -0,0 +1,47 @@
+#' Sparse Multiple Index (SMI) models
+#'
+#' Fits a nonparametric multiple index model to data with simultaneous variable 
+#' selection (hence "sparse").
+#'
+#' @param data Training data set on which models will be trained. Should be a
+#'   `tibble`.
+#' @param yvar Name of the response variable as a character string.
+#' @param index.vars A character vector of names of the predictor variables for
+#'   which indices should be estimated.
+#' @param index.ind An integer vector that assigns group index for each
+#'   predictor in `index.vars`. (The default is `NULL`. If `NULL`, the model
+#'   will be initialised with an Additive Model.)
+#' @param index.coefs A numeric vector of index coefficients (default: NULL).
+#' @param linear.vars A character vector of names of the predictor variables
+#'   that should be included linearly into the model.
+#' @param lambda0 Penalty parameter for L0 penalty.
+#' @param lambda2 Penalty parameter for L2 penalty.
+#' @param M Big-M value used in MIP.
+#' @param max.iter Maximum number of MIP iterations performed to update index
+#'   coefficients for a given model.
+#' @param tol Tolerance for loss.
+#' @param tolCoefs Tolerance for coefficients. 
+#' @param TimeLimit A limit for the total time (in seconds) expended in a single
+#'   MIP iteration.
+#' @param verbose The option to print detailed solver output.
+#'
+#' @export
+smimodel <- function(data, yvar, index.vars, index.ind = NULL, index.coefs = NULL, 
+                     linear.vars = NULL, lambda0 = 1, lambda2 = 1, 
+                     M = 10, max.iter = 50, tol = 0.001, tolCoefs = 0.001,
+                     TimeLimit = Inf, verbose = FALSE){
+  # Constructing the initial `smimodel`
+  init_smimodel <- new_smimodel(data = data, yvar = yvar, 
+                                index.vars = index.vars, 
+                                index.ind = index.ind, 
+                                index.coefs = index.coefs, 
+                                linear.vars = linear.vars)
+  # Optimising the initial `smimodel`
+  opt_smimodel <- update_smimodel(object = init_smimodel, data = data, 
+                                  lambda0 = lambda0, lambda2 = lambda2, 
+                                  M = M, max.iter = max.iter, 
+                                  tol = tol, tolCoefs = tolCoefs,
+                                  TimeLimit = TimeLimit, verbose = verbose)
+  output <- list("initial" = init_smimodel, "best" = opt_smimodel)
+  return(output)
+}

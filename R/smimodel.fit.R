6be7fd477d1869b194804886dd2243dca9f0f2e2
Nuwani Palihawadana <nuwanipalihawadana@gmail.com>
2024-04-15 16:58:41 +1000

    Correct algorithm to compare absolute coef values in PPR option

--- R/smimodel.fit.R
+++ R/smimodel.fit.R
@@ -92,11 +92,11 @@ smimodel.fit <- function(data, yvar, neighbour = 0,
                           nterms = num_ind)
     # Sparsifying indices
     ppr_coefs <- ppr_fit$alpha
-    threshold <- max(ppr_fit$alpha)*0.1
-    zero_ind <- which(ppr_fit$alpha < threshold)
+    threshold <- max(abs(ppr_fit$alpha))*0.1
+    zero_ind <- which(abs(ppr_fit$alpha) < threshold)
     ppr_coefs[zero_ind] <- 0 
     for(i in 1:NROW(ppr_coefs)){
-      maxCoef <- which.max(ppr_coefs[i, ])
+      maxCoef <- which.max(abs(ppr_coefs[i, ]))
       ppr_coefs[i, ][-maxCoef] <- 0 
     }
     index.ind <- vector(mode = "list", length = NCOL(ppr_coefs))

016bbaed61c0851aea23f1695578f50c081fd2e9
Nuwani Palihawadana <60213950+nuwani-palihawadana@users.noreply.github.com>
2024-08-20 22:12:18 +1000

    Add more forecast methods

--- R/forecast.R
+++ R/forecast.R
@@ -26,3 +26,41 @@ forecast.gaimFit <- function(object, h = 1, level = c(80, 95), newdata,
                         residuals = ts(residuals)), 
                    class = "forecast"))
 }
+
+
+#' @method forecast pprFit
+#'
+#' @export
+forecast.pprFit <- function(object, h = 1, level = c(80, 95), newdata, 
+                             recursive = FALSE, recursive_colRange = NULL, ...){
+  method <- "Projection Pursuit Regression Model"
+  pred <- predict(object = object, newdata = newdata, recursive = recursive,
+                  recursive_colRange = recursive_colRange)$.predict
+  fitted <- augment(object)$.fitted
+  residuals <- augment(object)$.resid
+  return(structure(list(method = method, 
+                        model = object, 
+                        mean = ts(pred), 
+                        fitted = ts(fitted), 
+                        residuals = ts(residuals)), 
+                   class = "forecast"))
+}
+
+
+#' @method forecast backward
+#'
+#' @export
+forecast.backward <- function(object, h = 1, level = c(80, 95), newdata, 
+                            recursive = FALSE, recursive_colRange = NULL, ...){
+  method <- "Nonparametric Additive Model with Backward Elimination"
+  pred <- predict(object = object, newdata = newdata, recursive = recursive,
+                  recursive_colRange = recursive_colRange)$.predict
+  fitted <- augment(object)$.fitted
+  residuals <- augment(object)$.resid
+  return(structure(list(method = method, 
+                        model = object, 
+                        mean = ts(pred), 
+                        fitted = ts(fitted), 
+                        residuals = ts(residuals)), 
+                   class = "forecast"))
+}

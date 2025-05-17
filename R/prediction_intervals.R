efd98bff4001ff4030c29f4b85ba08078fa9f5c7
Nuwani Palihawadana <60213950+nuwani-palihawadana@users.noreply.github.com>
2025-05-17 22:56:12 +1000

    Remove model saving from cb_cvforecast

--- R/prediction_intervals.R
+++ R/prediction_intervals.R
@@ -1076,7 +1076,7 @@ cb_cvforecast_smi <- function(object, data, yvar, neighbour = 0, predictor.vars,
   out$mean <- leadlagMat(pf, 1:h) |> window(start = time(pf)[nfirst + 1L])
   out$error <- leadlagMat(err, 1:h) |> window(start = time(err)[nfirst + 1L], end = time(err)[n])
   out$res <- res
-  out$modelFit <- modelFit
+  #out$modelFit <- modelFit
   out$level <- level
   out$cal_times <- cal_times
   out$num_cal <- num_cal
